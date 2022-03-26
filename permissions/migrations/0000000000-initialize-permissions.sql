af310961f9d0df27341ec7d0c99e6697834cb38f
David Cloutman <david.cloutman@lampfire.com>
2022-03-26 09:43:27 -0700

    Created a directory for SQL migration files. Added classic seven table permission schema to migrations.

---
+++ permissions/migrations/0000000000-initialize-permissions.sql
@@ -0,0 +1,60 @@
+CREATE TABLE `users` (
+  `user_id` binary(16) PRIMARY KEY DEFAULT (uuid()),
+  `username` varchar(255),
+  `created_at` timestamp
+);
+
+CREATE TABLE `user_group` (
+  `user_group_id` unsigned bigint PRIMARY KEY AUTO_INCREMENT,
+  `group_name` varchar(255),
+  `created_at` timestamp
+);
+
+CREATE TABLE `group_members` (
+  `user_id` binary(16),
+  `user_group_id` int,
+  `username` varchar(255),
+  `created_at` timestamp,
+  PRIMARY KEY (`user_id`, `user_group_id`)
+);
+
+CREATE TABLE `permissions` (
+  `permission_id` unsigned int PRIMARY KEY AUTO_INCREMENT,
+  `permission_token` varchar(255),
+  `title` varchar(255),
+  `notes` text
+);
+
+CREATE TABLE `permission_set_members` (
+  `permission_set_id` unsigned int,
+  `permission_id` unsigned int,
+  `permission_token` varchar(255),
+  `title` varchar(255),
+  `notes` text,
+  PRIMARY KEY (`permission_set_id`, `permission_id`)
+);
+
+CREATE TABLE `permission_grants` (
+  `user_group_id` unsigned int,
+  `permission_set_id` unsigned int,
+  PRIMARY KEY (`user_group_id`, `permission_set_id`)
+);
+
+CREATE TABLE `permissions_sets` (
+  `permission_set_id` unsigned int PRIMARY KEY,
+  `name` varchar(255),
+  `notes` text
+);
+
+ALTER TABLE `users` ADD FOREIGN KEY (`user_id`) REFERENCES `group_members` (`user_id`);
+
+ALTER TABLE `user_group` ADD FOREIGN KEY (`user_group_id`) REFERENCES `group_members` (`user_group_id`);
+
+ALTER TABLE `permission_set_members` ADD FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`permission_id`);
+
+ALTER TABLE `permission_set_members` ADD FOREIGN KEY (`permission_set_id`) REFERENCES `permissions_sets` (`permission_set_id`);
+
+ALTER TABLE `permission_grants` ADD FOREIGN KEY (`user_group_id`) REFERENCES `user_group` (`user_group_id`);
+
+ALTER TABLE `permission_grants` ADD FOREIGN KEY (`permission_set_id`) REFERENCES `permissions_sets` (`permission_set_id`);
+
