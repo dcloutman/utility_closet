6afba6cd6e644e2d22a7e57cde825bfaf69626c6
David Cloutman <david.cloutman@lampfire.com>
2022-08-02 09:52:10 -0700

    Added missing tables.

--- permissions/migrations/0000000000-initialize-permissions.sql
+++ permissions/migrations/0000000000-initialize-permissions.sql
@@ -27,12 +27,42 @@ CREATE TABLE `GroupMembers` (
    FOREIGN KEY (`user_group_id`)
    REFERENCES `UserGroups`(`user_group_id`)
 ) COMMENT="An entity that allows multiple groups to have multiple users and users to have multiple groups.";
- 
- 
- 
+
+
 CREATE TABLE `Permissions` (
  `permission_id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
  `permission_token` VARCHAR(256),
  `title` VARCHAR(256),
  `notes` TEXT
 ) COMMENT="A permission is a token (e.g. a string) that can be used programmatically to limit or constrain an application's behavior based on a user's association or non-association.";
+
+ 
+CREATE TABLE `PermissionSets` (
+ `permission_set_id` INT UNSIGNED PRIMARY KEY,
+ `permission_token` VARCHAR(256),
+ `title` VARCHAR(256),
+ `notes` TEXT
+) COMMENT="A permission set is a group of related permissions. This table provides a master record for each permision set.";
+ 
+CREATE TABLE `PermissionSetMembers` (
+ `permission_set_id` INT UNSIGNED,
+ `user_id` BINARY(16),
+ `notes` TEXT,
+ PRIMARY KEY (`permission_set_id`, `user_id`),
+ CONSTRAINT `FK2_user`
+   FOREIGN KEY (`user_id`) REFERENCES `Users`(`user_id`),
+ CONSTRAINT `FK1_permission_set`
+   FOREIGN KEY (`permission_set_id`) REFERENCES `PermissionSets`(`permission_set_id`)
+) COMMENT="Associates individual users with permission sets.";
+ 
+ 
+CREATE TABLE `PermissionSetGroupMembers` (
+ `user_group_id` BIGINT UNSIGNED,
+ `permission_set_id` INT UNSIGNED,
+ PRIMARY KEY (`user_group_id`, `permission_set_id`),
+ CONSTRAINT `FK2_user_group`
+   FOREIGN KEY (`user_group_id`) REFERENCES `UserGroups` (`user_group_id`),
+ CONSTRAINT `FK2_permission_set`
+   FOREIGN KEY (`permission_set_id`) REFERENCES `PermissionSets` (`permission_set_id`)
+) COMMENT="Associates user groups with permission sets.";
+ 
